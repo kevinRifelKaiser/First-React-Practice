dc59bb1003acd17f48dc539779e16714b4e33b76
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2022-05-22 20:39:46 -0300

    Add app, header, note and footer components

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div class="note">
+            <h1>Title note</h1>
+            <p>Content note</p>
+        </div>
+    );
+}
+
+export default Note;
