72ac5b38622aef4e316ca6155c68d5dbb40fefd9
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2022-05-29 12:04:14 -0300

    Add notes.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes"
+
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map(note => (
+                <Note
+                //acá es donde creamos las propiedades que seleccionamos luego, en Note.jsx 
+                key={note.key}
+                title={note.title}
+                content={note.content}
+            />
+            ))}            
             <Footer />
         </div>
     );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Note() {
+
+function Note(props) {
     return (
-        <div class="note">
-            <h1>Title note</h1>
-            <p>Content note</p>
+        <div className="note">
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     );
 }
