4a52e6890891aa3ba9a76c361dc31dbebcee72ca
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2022-06-08 18:39:13 -0300

    Create CreateArea component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,20 +3,15 @@ import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
 import notes from "../notes"
+import CreateArea from "./CreateArea";
 
 
 function App() {
     return (
         <div>
             <Header />
-            {notes.map(note => (
-                <Note
-                //acá es donde creamos las propiedades que seleccionamos luego, en Note.jsx 
-                key={note.key}
-                title={note.title}
-                content={note.content}
-            />
-            ))}            
+            <CreateArea />
+            <Note key={1} title="Note title" content="Note content" />          
             <Footer />
         </div>
     );
